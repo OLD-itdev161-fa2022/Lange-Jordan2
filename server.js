800dee531068a7a88922dfe4bb5ef7fb84fe5b96
Jordan Lange <langej11@gmatc.matc.edu>
2021-11-30 17:04:45 -0600

    completed activity-3

--- server.js
+++ server.js
@@ -1,16 +1,47 @@
-import express from "express";
-import connectDatabase from "./config/db";
+import express from 'express';
+import connectDatabase from './config/db';
+import {check, validationResult } from 'express-validator'
 
-//initilize express application
+//intalize express application
 const app = express();
 
 //connect database
 connectDatabase();
 
+//configure middleware
+app.use(express.json({ extended: false}));
+
 //api endpoints
+/**
+ * @route GET /
+ * @desc test endpoint
+ */
 app.get('/', (req, res) =>
     res.send('http get request sent to root api endpoint')
 );
 
+/**
+ * @route POST api/users
+ * @desc register user
+ */
+app.post('/api/users',
+[
+    check('name', 'Please enter your name')
+        .not()
+        .isEmpty(),
+    check('email', 'Please enter a valid email address')
+        .isEmail(),
+    check('password', 'Please enter a password with 6 or more characters')
+        .isLength({min: 6})
+], 
+(req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+        return res.status(422).json({ errors: errors.array() });
+    } else {
+        return res.send(req.body);
+    }
+});
+
 //connection listener
-app.listen(3000, () => console.log('Express server running on port 3000'));
+app.listen(3000, () => console.log ('Express server running on port 3000'));
