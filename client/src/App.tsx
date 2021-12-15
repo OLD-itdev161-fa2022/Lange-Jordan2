4c4fecac814a72cb523fc0b76883a206b841e7e6
Jordan Lange <langej11@gmatc.matc.edu>
2021-12-14 18:41:23 -0600

    Completed Activity 10

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,6 +6,8 @@ import Register from './components/Register/Register';
 import Login from './components/Login/Login';
 import PostList from './components/PostList/PostList';
 import Post from './components/Post/Post';
+import CreatePost from './components/Post/CreatePost';
+import EditPost from './components/Post/EditPost';
 
 class App extends React.Component {
   state = {
@@ -29,7 +31,7 @@ class App extends React.Component {
         }
       };
           axios
-            .get('http://localhost:5000')
+            .get('http://localhost:5000/api/posts',config)
             .then((response) => {
               this.setState({
                 posts: response.data
@@ -88,8 +90,58 @@ class App extends React.Component {
     this.setState({ user: null, token: null });
   }
 
+  deletePost = post => {
+    const { token } = this.state;
+
+    if (token) {
+      const config = {
+        headers: {
+          'x-auth-token': token
+        }
+      };
+
+      axios
+        .delete(`http://localhost:5000/api/posts/${post._id}`, config)
+        .then(response => {
+          const newPosts = this.state.posts.filter (p => p._id !== post._id);
+          this.setState({
+            posts: [...newPosts]
+          });
+        })
+        .catch(error => {
+          console.error(`Error deleting post: ${error}`);
+        });
+    }
+  };
+
+  editPost = post => {
+    this.setState ({
+      post: post
+    });
+  };
+
+onPostCreated = post => {
+  const newPosts = [...this.state.posts, post];
+
+  this.setState({
+    posts: newPosts
+  });
+};
+
+onPostUpdated = post => {
+  console.log('Updated post: ', post);
+  const newPosts = [...this.state.posts];
+  const index = newPosts.findIndex(p => p._id === post._id);
+
+  newPosts[index] = post;
+
+  this.setState({
+    posts: newPosts
+  });
+};
+
   render() {
-    let { user, posts, post } = this.state;
+    let { user, posts, post, token } = this.state;
     const authProps = {
       authenticateUser: this.authenticateUser
     };
@@ -101,40 +153,69 @@ class App extends React.Component {
             <h1>GoodThings</h1>
             <ul>
               <li>
-                <Link to="/">Home</Link>
+                <Link to ="/">Home</Link>
               </li>
               <li>
-                <Link to="/register">Register</Link>
+                {user ? (
+                  <Link to="/new-post">New Post</Link>
+                ) : (
+                  <Link to ="/register">Register</Link>
+                )}
               </li>
               <li>
-                {user ? 
-                  <Link to="" onClick={this.logOut}>Log out</Link> :
-                  <Link to="/login">Log in</Link> 
-                }
-                
+                {user ? (
+                  <Link to="" onClick={this.logOut}>
+                    Log Out
+                  </Link>
+                ) : (
+                  <Link to="/login">Log In</Link>
+                )}
               </li>
             </ul>
           </header>
           <main>
-            <Route exact path="/">
-              {user ? (
-                <React.Fragment>
-                  <div>Hello {user}!</div>
-                  <PostList posts={posts} clickPost={this.viewPost}/>
-                </React.Fragment> ) : (
-                <React.Fragment>
-                  Please Register or Login
-                </React.Fragment>
-                )}
-              
-            </Route>
             <Switch>
-              <Route 
-                exact path="/register" 
-                render={() => <Register {...authProps} />} />
-              <Route 
-                exact path="/login" 
-                render={() => <Login {...authProps} />} />
+              <Route exact path ="/">
+                {user ? (
+                  <React.Fragment>
+                    <div>Hello {user}!</div>
+                    <PostList 
+                      posts ={posts} 
+                      clickPost={this.viewPost}
+                      deletePost={this.deletePost}
+                      editPost={this.editPost}
+                    />
+                  </React.Fragment>
+                ): (
+                  <React.Fragment>Please Register or Login</React.Fragment>
+                )}
+              </Route>
+              <Route path="/posts/:postId">
+                <Post post={post}/>
+              </Route>
+              <Route path="/new-post">
+                <CreatePost 
+                  token ={token} 
+                  onPostCreated={this.onPostCreated}
+                />
+              </Route>
+              <Route path ="/edit-post/:postId">
+                <EditPost 
+                  token={token}
+                  post={post}
+                  onPostUpdated={this.onPostUpdated}
+                />
+              </Route>
+              <Route
+                exact
+                path="/register"
+                render={() => <Register {...authProps} />}
+              />
+              <Route
+                exact
+                path="/login"
+                render={() => <Login {...authProps}/>}
+              />
             </Switch>
           </main>
         </div>
